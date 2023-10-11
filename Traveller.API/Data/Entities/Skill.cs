426db31a0ddbe32f97035e913799ab578ef0dc76
David Carter <indysoftwaredev@gmail.com>
2023-10-11 18:33:45 -0400

    Adding Entity classes for Skills and SkillSpecializations

---
+++ Traveller.API/Data/Entities/Skill.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Traveller.API.Data.Entities
+{
+    public class Skill
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public ICollection<SkillSpecialization> SkillSpecializations { get; set; } = 
+            new List<SkillSpecialization>();
+
+    }
+}
---
+++ Traveller.API/Data/Entities/SkillSpecialization.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Traveller.API.Data.Entities
+{
+    public class SkillSpecialization
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ForeignKey("SkillId")]
+        public int SkillId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
